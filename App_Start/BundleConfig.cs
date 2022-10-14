ee440e73c1ee76cb787f59cd6536474d0fd6e70b
gdiazs <diazgbs@gmail.com>
2022-10-13 23:35:01 -0600

    Actualiza bootstrap configuracion

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,18 +8,12 @@ namespace ControlUniversitario
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new Bundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new Bundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            bundles.Add(new Bundle("~/scripts/core")
-                .Include("~/content/scripts/bootstrap.bundle.min.js"));
-
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.bundle.min.js"));
 
             bundles.Add(new Bundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.min.css",
                       "~/Content/site.css"));
         }
     }
